1ba20a57c2b774dcac7c3266468c27169ee7650b
Quinn Schwartz <quinnschwartz@gmail.com>
2017-01-19 11:22:13 -0800

    refactor edit function across multiple components.

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,23 +7,23 @@ import { Task } from './task.model';
   <div class="container">
    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
    <h3>{{currentFocus}}</h3>
-   <task-list></task-list>
+   <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
    <hr>
   <div>
     <div *ngIf="selectedTask">
-   <h3>{{selectedTask.description}}</h3>
-   <p>Task Complete? {{selectedTask.done}}</p>
-  <h3>Edit Task</h3>
-  <label>Enter Task Description:</label>
-  <input [(ngModel)]="selectedTask.description">
-   <label>Enter Task Priority (1-3):</label>
-   <br>
-   <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-   <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-   <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+    </div>
+   </div>
   </div>
- </div>
-</div>
 `
 })
 
@@ -34,6 +34,12 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   selectedTask: null;
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
